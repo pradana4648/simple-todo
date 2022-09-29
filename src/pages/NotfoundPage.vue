2928d159b4338bb86cf57b8c6e312a5da15e139f
pradana4648 <wakwaujaim@gmail.com>
2022-09-29 21:08:26 +0700

    feat: added notfound route page

---
+++ src/pages/NotfoundPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <div class="text-xl font-bold">Halaman tidak ditemukan</div>
+    <router-link to="/" class="my-5">Ke Halaman Awal</router-link>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
